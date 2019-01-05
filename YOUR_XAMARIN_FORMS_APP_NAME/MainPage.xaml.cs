079aeca40705bbbd330eb460913e53340b0c6716
Leanne Cabrera <little.corner.dev@gmail.com>
2019-01-05 14:08:13 -0500

    Initial commit: default blank forms app

---
+++ YOUR_XAMARIN_FORMS_APP_NAME/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace YOUR_XAMARIN_FORMS_APP_NAME {
+	public partial class MainPage : ContentPage {
+		public MainPage() {
+			InitializeComponent();
+		}
+	}
+}
